035e199bbc5280f26733955fe8b4cc6920a57230
issyrivas17 <ismeniarivas17@gmail.com>
2021-07-27 22:31:02 -0400

    PropTypes

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,9 +1,10 @@
    import React from 'react';
+   import PropTypes from 'prop-types'; 
    
    /*  import React, { Fragment } from 'react';  */
 
-    const PrimeraApp = ({saludo ="Hola amigos! Bienvenidos a counter."}) => {
-
+    const PrimeraApp = ({saludo}) => {
+     
         return <>
         <h1>{saludo}</h1>
            {/*  <pre>{JSON.stringify(saludo,null,3)}</pre>  */}
@@ -11,5 +12,8 @@
                                                          
         </>;
         } 
-
+        
+        PrimeraApp.protoTypes ={
+            saludo: PropTypes.string.isRequired
+        }
         export default PrimeraApp;  
