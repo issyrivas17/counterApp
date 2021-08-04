f17bb652e6179a4866db8d46fb7e5555ee5e3fd3
issyrivas17 <ismeniarivas17@gmail.com>
2021-08-03 21:53:58 -0400

    Haciendo primera prueba oficial con expectTobe

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,9 +1,23 @@
-test ('esta es mi primera prueba', () => {
-
-const isActive = true; 
-
-if (isActive) {
-    throw new Error ('No esta activo'); 
-}
+describe('Pruebas en el archivo demo.test.js', () => {
+    
+    test ('deben de ser iguales los dos string', () => {
+        // 1.Inicializacion 
+        const mensaje = 'Hola mundo'; 
+        
+        // 2. Estimulo 
+        const mensaje2= "Hola mundo"; 
+        
+        //3. Observar el comportamiento 
+        
+        expect (mensaje).toBe(mensaje2); // == 
+        
+        
+        
+        });
 
 });
+
+
+
+
+
