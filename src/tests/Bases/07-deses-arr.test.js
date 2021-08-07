74b0fd0c6fa56715d9c85beaec90c7a7e4d54930
issyrivas17 <ismeniarivas17@gmail.com>
2021-08-06 20:41:51 -0400

    Pruebas con 07-deses-arr

---
+++ src/tests/Bases/07-deses-arr.test.js
@@ -0,0 +1,24 @@
+import { retornaArreglo } from "../../base-pruebas/07-deses-arr";
+
+describe('Pruebas en desestructuracion', () => {
+    
+    test('Debe de retornar un string y un numero }', () => {
+      
+        const [letras,numeros] = retornaArreglo (); 
+    
+      console.log(typeof numeros)
+
+     
+
+       expect(letras).toBe('ABC')
+       expect(typeof letras).toBe('string') 
+
+       expect(numeros).toBe(123)
+       expect(typeof numeros).toBe('number')
+
+
+    })
+    
+
+
+});
