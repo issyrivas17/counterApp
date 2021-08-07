7bedc951e1247018cc6f1bf19aab9f905d0f1bf3
issyrivas17 <ismeniarivas17@gmail.com>
2021-08-06 21:22:56 -0400

    Hay un error en uno de los modulos, se debe ver el codigo y verificar que es lo que sucede

--- src/tests/Bases/08-imp-exp.test.js
+++ src/tests/Bases/08-imp-exp.test.js
@@ -1,16 +1,25 @@
-import { getHeroeById } from "../../base-pruebas/08-imp-exp";
+import { getHeroeById, getHeroesByOwner } from "../../base-pruebas/08-imp-exp";
 import heroes from "../../data/heroes";
 
 describe('Pruebas en funciones de heroes', () => {
     
-test('debe de retornar un heroe por id ', () => {
+test('debe de retornar un undefined si heroe no existe  ', () => {
     
-    const id = 1; 
-    const heroe = getHeroeById(id); 
+    const id = 10; 
+    const heroes = getHeroeById(id); 
+    expect (heroes).toBe(undefined); 
+}) 
 
-    const heroeData= heroes.find(heroe =>heroe.id === id);  
 
-    expect (heroe).toEqual(heroeData)
+test('Debe retornar un arreglo con los heroes de DC', () => { 
+
+    const owner = 'DC' 
+    const heroes = getHeroesByOwner (owner); 
+
+   const heroesData = heroes.filter( (heroe) => heroe.owner === owner ); 
+
+   expect(heroes).toEqual(heroesData)
 })
+
 
 });
