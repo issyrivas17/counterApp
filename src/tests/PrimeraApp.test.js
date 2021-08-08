fedd110d2c793d11abc33254807f2731ac2e183d
issyrivas17 <ismeniarivas17@gmail.com>
2021-08-08 16:42:21 -0400

    Se importo Enzyme pero falla cuando voy a pasar las pruebas,no fue de la documentacion oficial, tiene fallas

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { render } from "@testing-library/react";
-import PrimeraApp from "../PrimeraApp";
+import { shallow } from "@wojtekmaj/enzyme-adapter-react-17";
+import PrimeraApp from "../PrimeraApp"; 
+import '@testing-library/jest-dom'
 
 describe('Pruebas en <PrimeraApp/>', () => { 
- test('Debe mostrar el mensaje de "Hola,soy ismenia"', () => {
+/*  test('Debe mostrar el mensaje de "Hola,soy ismenia"', () => {
      const saludo = 'Hola,soy ismenia'; 
      const {getByText}= render(<PrimeraApp saludo ={saludo}/>)
 
     expect(getByText(saludo) ).toBeInTheDocument(); 
  })
  
+     */ 
 
-
+test('Debe de mostrar <PrimeraApp/> correctamente', () => {
     
+    const saludo= 'Hola,soy ismenia'; 
+const wrapper=shallow(<PrimeraApp saludo={saludo} />) 
+
+expect (wrapper).toMatchSnapshot(); 
+
+
+
+})
+
+
+
 });
