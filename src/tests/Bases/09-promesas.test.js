afb88f33d9a69159a672624322a4c2ee46c6f590
issyrivas17 <ismeniarivas17@gmail.com>
2021-08-08 15:19:27 -0400

    Pruebas asincronas

---
+++ src/tests/Bases/09-promesas.test.js
@@ -0,0 +1,32 @@
+import { getHeroeByIdAsync } from "../../base-pruebas/09-promesas";
+import heroes from "../../data/heroes";
+
+
+describe('Pruebas con promesas', (done) => {
+    test('Debe retornar un heroe async', () => {
+        const id = 1; 
+       
+            getHeroeByIdAsync (id) 
+            .then (heroe => {
+
+                expect(heroe).toBe(heroes)
+            done();
+
+            })
+        
+    })  
+
+    test('Debe obtener un error si el heroe no existe', (done) => {
+        
+        const id= 10; 
+
+        getHeroeByIdAsync(id) 
+        .catch(error => {
+            expect (error).toBe('No se puede encontrar el heroe')
+        })
+
+        done(); 
+    })
+    
+    
+});
