be06cba2088c8316ab32cf6de6399be98b2eeff7
issyrivas17 <ismeniarivas17@gmail.com>
2021-08-08 11:27:25 -0400

    Solucionado el error, prueba unitaria de 08impexp lista

--- src/tests/Bases/08-imp-exp.test.js
+++ src/tests/Bases/08-imp-exp.test.js
@@ -1,25 +1,40 @@
 import { getHeroeById, getHeroesByOwner } from "../../base-pruebas/08-imp-exp";
 import heroes from "../../data/heroes";
-
+ 
 describe('Pruebas en funciones de heroes', () => {
+    test('Debe retornar un heroe por id', () => {
+        const id = 1; 
+        const heroe = getHeroeById (id); 
+
+     const heroeData= heroes.find(heroes => heroes.id === id); 
+
+     expect(heroe).toEqual(heroeData)
+
+    })
     
-test('debe de retornar un undefined si heroe no existe  ', () => {
-    
-    const id = 10; 
-    const heroes = getHeroeById(id); 
-    expect (heroes).toBe(undefined); 
-}) 
 
+    test('Debe retornar undefined si heroe no existe', () => {
+        const id = 10; 
+        const heroe = getHeroeById (id); 
 
-test('Debe retornar un arreglo con los heroes de DC', () => { 
+     expect(heroe).toBe(undefined)
 
-    const owner = 'DC' 
-    const heroes = getHeroesByOwner (owner); 
+    }) 
 
-   const heroesData = heroes.filter( (heroe) => heroe.owner === owner ); 
+test('Debe de retornar uno de los heroes de Dc', () => { 
+    const owner= 'DC'; 
+    const heroes = getHeroesByOwner (owner) 
 
-   expect(heroes).toEqual(heroesData)
-})
+     const heroesData= heroes.filter(heroes =>heroes.owner === owner);
 
+     expect (heroes).toEqual(heroesData)
 
-});
+}); 
+ test('Debe retornar un arreglo de los heroes de Marvel', () => {
+     const owner = 'Marvel'; 
+     const heroes= getHeroesByOwner(owner) 
+
+     expect(heroes.length).toBe(2)
+ })
+ 
+}); 
